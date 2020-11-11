d715a51af391dc4f17734245bf6eded528e2d795
tjinlag <tjinlag@gmail.com>
2020-11-11 08:57:04 +0700

    update path of data

--- services/telco.js
+++ services/telco.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 export const getCards = async () => {
-  const rawdata = fs.readFileSync('data/card.json');
+  const rawdata = fs.readFileSync('./data/card.json');
   const data = JSON.parse(rawdata);
 
   return data;
