c1ef57b557503fd0bb78b95a0042ebb4d4e8f345
tjinlag <tjinlag@gmail.com>
2020-11-16 22:18:13 +0700

    test

--- pages/api/cards.js
+++ pages/api/cards.js
@@ -1,9 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 const fs = require('fs');
+const path = require('path');
 const { join } = require('path');
 
 export default (req, res) => {
+  console.log(path.resolve('./public', 'data'));
+  console.log(join(path.resolve('./public', 'data'), 'card.json'));
   const rawdata = fs.readFileSync(join(path.resolve('./public', 'data'), 'card.json'));
   const data = JSON.parse(rawdata);
 
