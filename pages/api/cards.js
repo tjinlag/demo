9fc5fd0ed8ec7dc141f7f136e371409d0df0cd6c
tjinlag <tjinlag@gmail.com>
2020-11-16 22:21:28 +0700

    update path

--- pages/api/cards.js
+++ pages/api/cards.js
@@ -5,9 +5,8 @@ const path = require('path');
 const { join } = require('path');
 
 export default (req, res) => {
-  console.log(path.resolve('./public', 'data'));
-  console.log(join(path.resolve('./public', 'data'), 'card.json'));
-  const rawdata = fs.readFileSync(join(path.resolve('./public', 'data'), 'card.json'));
+  const dir = path.resolve('./public', 'data');
+  const rawdata = fs.readFileSync(join(dir, 'card.json'));
   const data = JSON.parse(rawdata);
 
   res.statusCode = 200;
